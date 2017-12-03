d45c613212fb3b38410562238a42907a6a301dbe
NolanMullins <nolan.mullins@me.com>
2017-12-03 14:27:18 -0500

    worked on new style, started customizing

---
+++ new/js/extra.js
@@ -0,0 +1,24 @@
+$(".scroll-menu li a[href^='#']").on('click', function(e) {
+
+    // This sets the hash
+    var target;
+    target = this.hash;
+
+    // Prevent default anchor click behavior
+    e.preventDefault();
+
+    // The grabs the height of my header
+    var navOffset;
+    navOffset = $('#header').height();
+
+    // Animate The Scroll
+    $('html, body').animate({
+        scrollTop: $(this.hash).offset().top - navOffset
+    }, 600, function(){
+
+    // Adds hash to end of URL
+    return window.history.pushState(null, null, target);
+
+    });
+
+});
